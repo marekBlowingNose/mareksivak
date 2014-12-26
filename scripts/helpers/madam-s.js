fc6c149e0933e8e1225c07423762a42b80ef3f4a
marekBlowingNose <marek@blowingnose.com>
2014-12-26 15:17:28 +0100

    Animations

--- scripts/helpers/madam-s.js
+++ scripts/helpers/madam-s.js
@@ -1,4 +1,17 @@
 
+/* analytics*/
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-20730989-3', 'auto');
+  ga('send', 'pageview');
+
+/* end of analytics*/
+
+
 
 
 function contactMe(element) {
@@ -31,6 +44,11 @@ function contactMe(element) {
 		};
 
 
+$('.intro h1').css("opacity", "0"); 
+$(".nav").css("left", "-250px").css("opacity", "0");
+$(".intro .btn").css("bottom", "-50px").css("opacity", "0");
+$(".work").css("opacity", "0");
+$(".about").css("opacity", "0");
 
 $(document).ready(function() {
 
@@ -39,12 +57,85 @@ $(document).ready(function() {
 	
 	$('textarea').autosize();  
 	
-	$(".intro h1").hide().delay(200).fadeIn(400);
+	//$(".intro h1").hide().delay(200).fadeIn(400);
+	
+    
+    
+    
+    
+    $('.intro .deco').addClass('animated900 scaleInY');
+    
+    $('.intro .deco').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',  function()
+   {
+        $('.intro .deco').removeClass('animated900 scaleInY');
+        
+        //
+   });
+    
+    
+    setTimeout( function() {
+       $('.intro h1').css("opacity", "1"); 
+        $('.intro h1').addClass('animated900 moveInLeftShort');
+   }, 800);
+    
+    
+
 	//$(".nav").css("top", "-50px").css("opacity", "0").delay( 500 ).animate({top: "-15px"}, 300).animate({top: "20px", opacity: "1"}, 200);
-	$(".nav").css("left", "-250px").css("opacity", "0").delay( 500 ).animate({left: "0px"}, 300).animate({left: "20px", opacity: "1"}, 200);
-	$(".intro .btn").css("bottom", "-50px").css("opacity", "0").delay( 1000 ).animate({bottom: "-15px"}, 300).animate({bottom: "0px", opacity: "1"}, 200);
+	$(".nav").delay( 1500 ).animate({left: "0px"}, 300).animate({left: "20px", opacity: "1"}, 200);
+	$(".intro .btn").delay( 2000 ).animate({bottom: "-15px"}, 300).animate({bottom: "0px", opacity: "1"}, 200);
 	
-	
+
+    
+    $('.work, #contact-form')
+				.waypoint({
+					triggerOnce: true,
+					handler: function(direction)
+					{
+						if (direction == 'down')
+						{
+							var $this = $(this);
+                            
+                                $this.css('opacity','1');
+                                $this.addClass('animated1100 moveInLeftShort');
+                          
+                            $this.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',  function()
+                           {
+                                 $this.removeClass('animated1100 moveInLeftShort');
+                           });
+                            
+
+						}
+					},
+					offset: '85%'
+				});
+        
+    $('.about, #direct-mail')
+				.waypoint({
+					triggerOnce: true,
+					handler: function(direction)
+					{
+						if (direction == 'down')
+						{
+							var $this = $(this);
+                            
+                                $this.css('opacity','1');
+                                $this.addClass('animated1100 moveInRightShort');
+                          
+                            $this.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',  function()
+                           {
+                                 $this.removeClass('animated1100 moveInRightShort');
+                           });
+                            
+
+						}
+					},
+					offset: '85%'
+				});
+    
+    // smooth scroll to target element
+   		
+    
+    
 	smoothScroll.init({
 				speed: 1000,
 				easing: 'easeInOutCubic',
@@ -54,6 +145,22 @@ $(document).ready(function() {
 				callbackAfter: function ( toggle, anchor ) {}
 	});
 });
+
+
+$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		      }
+		    }
+		  });
+		});
 
 
 $(document).scroll(function(){
